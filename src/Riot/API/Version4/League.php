b32b3e194e177476d523ba20eb52ed97647e848a
simivar <simivar@gmail.com>
2020-10-25 00:40:46 +0200

    Implement all League v4 endpoints

--- src/Riot/API/Version4/League.php
+++ src/Riot/API/Version4/League.php
@@ -20,4 +20,34 @@ final class League extends AbstractApi
 
         return LeagueListDTO::createFromArray($response->getBodyContentsDecodedAsArray());
     }
+
+    public function getGrandmasterLeaguesByQueue(QueueEnum $queue, RegionEnum $region): LeagueListDTO
+    {
+        $response = $this->riotConnection->get(
+            $region->__toString(),
+            sprintf('lol/league/v4/grandmasterleagues/by-queue/%s', $queue->__toString()),
+        );
+
+        return LeagueListDTO::createFromArray($response->getBodyContentsDecodedAsArray());
+    }
+
+    public function getById(string $leagueId, RegionEnum $region): LeagueListDTO
+    {
+        $response = $this->riotConnection->get(
+            $region->__toString(),
+            sprintf('lol/league/v4/leagues/%s', $leagueId),
+        );
+
+        return LeagueListDTO::createFromArray($response->getBodyContentsDecodedAsArray());
+    }
+
+    public function getMasterLeaguesByQueue(QueueEnum $queue, RegionEnum $region): LeagueListDTO
+    {
+        $response = $this->riotConnection->get(
+            $region->__toString(),
+            sprintf('lol/league/v4/masterleagues/by-queue/%s', $queue->__toString()),
+        );
+
+        return LeagueListDTO::createFromArray($response->getBodyContentsDecodedAsArray());
+    }
 }
--- tests/Unit/API/Version4/LeagueTest.php
+++ tests/Unit/API/Version4/LeagueTest.php
@@ -28,4 +28,58 @@ final class LeagueTest extends APITestCase
         $result = $league->getChallengerLeaguesByQueue(QueueEnum::RANKED_SOLO_5x5(), RegionEnum::EUN1());
         self::assertInstanceOf(LeagueListDTO::class, $result);
     }
+
+    public function testGetGrandmasterLeaguesByQueueReturnsProperObjectOnSuccess(): void
+    {
+        $league = new League($this->createObjectConnectionMock(
+            'lol/league/v4/grandmasterleagues/by-queue/RANKED_SOLO_5x5',
+            [
+                'tier' => 'GRANDMASTER',
+                'leagueId' => '12345678-1ba0-3357-829e-4793b7be8601',
+                'queue' => 'RANKED_SOLO_5x5',
+                'name' => "Renekton's Maulers",
+                'entries' => [],
+            ],
+            'eun1',
+        ));
+        $result = $league->getGrandmasterLeaguesByQueue(QueueEnum::RANKED_SOLO_5x5(), RegionEnum::EUN1());
+        self::assertInstanceOf(LeagueListDTO::class, $result);
+    }
+
+    public function testGeyByIdReturnsProperObjectOnSuccess(): void
+    {
+        $league = new League($this->createObjectConnectionMock(
+            'lol/league/v4/leagues/1',
+            [
+                'tier' => 'GRANDMASTER',
+                'leagueId' => '12345678-1ba0-3357-829e-4793b7be8601',
+                'queue' => 'RANKED_SOLO_5x5',
+                'name' => "Renekton's Maulers",
+                'entries' => [],
+            ],
+            'eun1',
+        ));
+        $result = $league->getById(
+            '1',
+            RegionEnum::EUN1(),
+        );
+        self::assertInstanceOf(LeagueListDTO::class, $result);
+    }
+
+    public function testGetMasterLeaguesByQueueReturnsProperObjectOnSuccess(): void
+    {
+        $league = new League($this->createObjectConnectionMock(
+            'lol/league/v4/masterleagues/by-queue/RANKED_SOLO_5x5',
+            [
+                'tier' => 'GRANDMASTER',
+                'leagueId' => '12345678-1ba0-3357-829e-4793b7be8601',
+                'queue' => 'RANKED_SOLO_5x5',
+                'name' => "Renekton's Maulers",
+                'entries' => [],
+            ],
+            'eun1',
+        ));
+        $result = $league->getMasterLeaguesByQueue(QueueEnum::RANKED_SOLO_5x5(), RegionEnum::EUN1());
+        self::assertInstanceOf(LeagueListDTO::class, $result);
+    }
 }
