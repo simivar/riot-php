0b260e4c46554046bf814ab94f5e032414a95a2a
simivar <simivar@gmail.com>
2020-10-25 20:04:50 +0100

    Implement Match v4 getIdsByTournamentCode and getByMatchIdAndTournamentCode endpoints

--- src/Riot/API/Version4/Match.php
+++ src/Riot/API/Version4/Match.php
@@ -4,9 +4,12 @@ declare(strict_types=1);
 
 namespace Riot\API\Version4;
 
+use JsonException;
+use Psr\Http\Client\ClientExceptionInterface;
 use Riot\API\AbstractApi;
 use Riot\DTO\Lol\MatchDTO;
 use Riot\Enum\RegionEnum;
+use Riot\Exception as RiotException;
 
 final class Match extends AbstractApi
 {
@@ -15,6 +18,43 @@ final class Match extends AbstractApi
         $response = $this->riotConnection->get(
             $region->__toString(),
             sprintf('lol/match/v4/matches/%s', $matchId),
+        );
+
+        return MatchDTO::createFromArray($response->getBodyContentsDecodedAsArray());
+    }
+
+    /**
+     * @return array<int, int>
+     *
+     * @throws JsonException
+     * @throws RiotException\BadGatewayException
+     * @throws RiotException\BadRequestException
+     * @throws RiotException\DataNotFoundException
+     * @throws RiotException\ForbiddenException
+     * @throws RiotException\GatewayTimeoutException
+     * @throws RiotException\InternalServerErrorException
+     * @throws RiotException\MethodNotAllowedException
+     * @throws RiotException\RateLimitExceededException
+     * @throws RiotException\ServiceUnavailableException
+     * @throws RiotException\UnauthorizedException
+     * @throws RiotException\UnsupportedMediaTypeException
+     * @throws ClientExceptionInterface
+     */
+    public function getIdsByTournamentCode(string $tournamentCode, RegionEnum $region): array
+    {
+        $response = $this->riotConnection->get(
+            $region->__toString(),
+            sprintf('lol/match/v4/matches/by-tournament-code/%s/ids', $tournamentCode),
+        );
+
+        return $response->getBodyContentsDecodedAsArray();
+    }
+
+    public function getByMatchIdAndTournamentCode(int $matchId, string $tournamentCode, RegionEnum $region): MatchDTO
+    {
+        $response = $this->riotConnection->get(
+            $region->__toString(),
+            sprintf('lol/match/v4/matches/%s/by-tournament-code/%s', $matchId, $tournamentCode),
         );
 
         return MatchDTO::createFromArray($response->getBodyContentsDecodedAsArray());
--- tests/Unit/API/Version4/MatchTest.php
+++ tests/Unit/API/Version4/MatchTest.php
@@ -11,7 +11,7 @@ use Riot\Tests\APITestCase;
 
 final class MatchTest extends APITestCase
 {
-    public function testGetChallengerLeaguesByQueueReturnsProperObjectOnSuccess(): void
+    public function testGetByMatchIdReturnsProperObjectOnSuccess(): void
     {
         $league = new Match($this->createObjectConnectionMock(
             'lol/match/v4/matches/1',
@@ -33,6 +33,46 @@ final class MatchTest extends APITestCase
             'eun1',
         ));
         $result = $league->getByMatchId(1, RegionEnum::EUN1());
+        self::assertInstanceOf(MatchDTO::class, $result);
+    }
+
+    public function testGetIdsByTournamentCodeProperObjectOnSuccess(): void
+    {
+        $league = new Match($this->createObjectConnectionMock(
+            'lol/match/v4/matches/by-tournament-code/1/ids',
+            [
+                1234567890
+            ],
+            'eun1',
+        ));
+        $result = $league->getIdsByTournamentCode('1', RegionEnum::EUN1());
+        self::assertIsArray($result);
+        self::assertArrayHasKey(0, $result);
+        self::assertSame(1234567890, $result[0]);
+    }
+
+    public function testGetByMatchIdAndTournamentCodeReturnsProperObjectOnSuccess(): void
+    {
+        $league = new Match($this->createObjectConnectionMock(
+            'lol/match/v4/matches/1/by-tournament-code/1',
+            [
+                'gameId' => 1234567890,
+                'platformId' => 'EUN1',
+                'gameCreation' => 1603575904886,
+                'gameDuration' => 2441,
+                'queueId' => 430,
+                'mapId' => 11,
+                'seasonId' => 13,
+                'gameVersion' => '10.21.339.2173',
+                'gameMode' => 'CLASSIC',
+                'gameType' => 'MATCHED_GAME',
+                'teams' => [],
+                'participants' => [],
+                'participantIdentities' => [],
+            ],
+            'eun1',
+        ));
+        $result = $league->getByMatchIdAndTournamentCode(1, '1', RegionEnum::EUN1());
         self::assertInstanceOf(MatchDTO::class, $result);
     }
 }
