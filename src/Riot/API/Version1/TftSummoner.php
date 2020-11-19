5574148b005bb5b2220295e5fea7476977d9f99a
simivar <simivar@gmail.com>
2020-11-19 22:42:32 +0100

    Implement all TFT Summoner v1 endpoints

---
+++ src/Riot/API/Version1/TftSummoner.php
@@ -0,0 +1,117 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Riot\API\Version1;
+
+use JsonException;
+use Psr\Http\Client\ClientExceptionInterface;
+use Riot\API\AbstractApi;
+use Riot\DTO\LeaderboardDTO;
+use Riot\DTO\SummonerDTO;
+use Riot\Enum\GeoRegionEnum;
+use Riot\Enum\RegionEnum;
+use Riot\Exception as RiotException;
+
+final class TftSummoner extends AbstractApi
+{
+    /**
+     * @throws JsonException
+     * @throws RiotException\BadGatewayException
+     * @throws RiotException\BadRequestException
+     * @throws RiotException\DataNotFoundException
+     * @throws RiotException\ForbiddenException
+     * @throws RiotException\GatewayTimeoutException
+     * @throws RiotException\InternalServerErrorException
+     * @throws RiotException\MethodNotAllowedException
+     * @throws RiotException\RateLimitExceededException
+     * @throws RiotException\ServiceUnavailableException
+     * @throws RiotException\UnauthorizedException
+     * @throws RiotException\UnsupportedMediaTypeException
+     * @throws ClientExceptionInterface
+     */
+    public function getByAccountId(string $encryptedAccountId, RegionEnum $region): SummonerDTO
+    {
+        $response = $this->riotConnection->get(
+            $region->__toString(),
+            sprintf('tft/summoner/v1/summoners/by-account/%s', $encryptedAccountId),
+        );
+
+        return SummonerDTO::createFromArray($response->getBodyContentsDecodedAsArray());
+    }
+
+    /**
+     * @throws JsonException
+     * @throws RiotException\BadGatewayException
+     * @throws RiotException\BadRequestException
+     * @throws RiotException\DataNotFoundException
+     * @throws RiotException\ForbiddenException
+     * @throws RiotException\GatewayTimeoutException
+     * @throws RiotException\InternalServerErrorException
+     * @throws RiotException\MethodNotAllowedException
+     * @throws RiotException\RateLimitExceededException
+     * @throws RiotException\ServiceUnavailableException
+     * @throws RiotException\UnauthorizedException
+     * @throws RiotException\UnsupportedMediaTypeException
+     * @throws ClientExceptionInterface
+     */
+    public function getByName(string $summonerName, RegionEnum $region): SummonerDTO
+    {
+        $response = $this->riotConnection->get(
+            $region->__toString(),
+            sprintf('tft/summoner/v1/summoners/by-name/%s', $summonerName),
+        );
+
+        return SummonerDTO::createFromArray($response->getBodyContentsDecodedAsArray());
+    }
+
+    /**
+     * @throws JsonException
+     * @throws RiotException\BadGatewayException
+     * @throws RiotException\BadRequestException
+     * @throws RiotException\DataNotFoundException
+     * @throws RiotException\ForbiddenException
+     * @throws RiotException\GatewayTimeoutException
+     * @throws RiotException\InternalServerErrorException
+     * @throws RiotException\MethodNotAllowedException
+     * @throws RiotException\RateLimitExceededException
+     * @throws RiotException\ServiceUnavailableException
+     * @throws RiotException\UnauthorizedException
+     * @throws RiotException\UnsupportedMediaTypeException
+     * @throws ClientExceptionInterface
+     */
+    public function getByPuuid(string $encryptedPuuid, RegionEnum $region): SummonerDTO
+    {
+        $response = $this->riotConnection->get(
+            $region->__toString(),
+            sprintf('tft/summoner/v1/summoners/by-puuid/%s', $encryptedPuuid),
+        );
+
+        return SummonerDTO::createFromArray($response->getBodyContentsDecodedAsArray());
+    }
+
+    /**
+     * @throws JsonException
+     * @throws RiotException\BadGatewayException
+     * @throws RiotException\BadRequestException
+     * @throws RiotException\DataNotFoundException
+     * @throws RiotException\ForbiddenException
+     * @throws RiotException\GatewayTimeoutException
+     * @throws RiotException\InternalServerErrorException
+     * @throws RiotException\MethodNotAllowedException
+     * @throws RiotException\RateLimitExceededException
+     * @throws RiotException\ServiceUnavailableException
+     * @throws RiotException\UnauthorizedException
+     * @throws RiotException\UnsupportedMediaTypeException
+     * @throws ClientExceptionInterface
+     */
+    public function getById(string $id, RegionEnum $region): SummonerDTO
+    {
+        $response = $this->riotConnection->get(
+            $region->__toString(),
+            sprintf('tft/summoner/v1/summoners/%s', $id),
+        );
+
+        return SummonerDTO::createFromArray($response->getBodyContentsDecodedAsArray());
+    }
+}
