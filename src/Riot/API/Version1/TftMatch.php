c82bc076678429146643c497285517b2ba5da995
simivar <simivar@gmail.com>
2020-11-20 01:08:11 +0100

    Implement all Tft Match v1 endpoints

--- src/Riot/API/Version1/TftMatch.php
+++ src/Riot/API/Version1/TftMatch.php
@@ -4,28 +4,31 @@ declare(strict_types=1);
 
 namespace Riot\API\Version1;
 
+use JsonException;
+use Psr\Http\Client\ClientExceptionInterface;
 use Riot\API\AbstractApi;
 use Riot\DTO\Tft\MatchDTO;
 use Riot\Enum\GeoRegionEnum;
+use Riot\Exception as RiotException;
 
 final class TftMatch extends AbstractApi
 {
     /**
      * @return array<string>
      *
-     * @throws \JsonException
-     * @throws \Psr\Http\Client\ClientExceptionInterface
-     * @throws \Riot\Exception\BadGatewayException
-     * @throws \Riot\Exception\BadRequestException
-     * @throws \Riot\Exception\DataNotFoundException
-     * @throws \Riot\Exception\ForbiddenException
-     * @throws \Riot\Exception\GatewayTimeoutException
-     * @throws \Riot\Exception\InternalServerErrorException
-     * @throws \Riot\Exception\MethodNotAllowedException
-     * @throws \Riot\Exception\RateLimitExceededException
-     * @throws \Riot\Exception\ServiceUnavailableException
-     * @throws \Riot\Exception\UnauthorizedException
-     * @throws \Riot\Exception\UnsupportedMediaTypeException
+     * @throws JsonException
+     * @throws RiotException\BadGatewayException
+     * @throws RiotException\BadRequestException
+     * @throws RiotException\DataNotFoundException
+     * @throws RiotException\ForbiddenException
+     * @throws RiotException\GatewayTimeoutException
+     * @throws RiotException\InternalServerErrorException
+     * @throws RiotException\MethodNotAllowedException
+     * @throws RiotException\RateLimitExceededException
+     * @throws RiotException\ServiceUnavailableException
+     * @throws RiotException\UnauthorizedException
+     * @throws RiotException\UnsupportedMediaTypeException
+     * @throws ClientExceptionInterface
      */
     public function getByPuuid(string $puuid, GeoRegionEnum $geoRegion, int $count = 20): array
     {
@@ -37,6 +40,21 @@ final class TftMatch extends AbstractApi
         return $response->getBodyContentsDecodedAsArray();
     }
 
+    /**
+     * @throws JsonException
+     * @throws RiotException\BadGatewayException
+     * @throws RiotException\BadRequestException
+     * @throws RiotException\DataNotFoundException
+     * @throws RiotException\ForbiddenException
+     * @throws RiotException\GatewayTimeoutException
+     * @throws RiotException\InternalServerErrorException
+     * @throws RiotException\MethodNotAllowedException
+     * @throws RiotException\RateLimitExceededException
+     * @throws RiotException\ServiceUnavailableException
+     * @throws RiotException\UnauthorizedException
+     * @throws RiotException\UnsupportedMediaTypeException
+     * @throws ClientExceptionInterface
+     */
     public function getById(string $matchId, GeoRegionEnum $geoRegion): MatchDTO
     {
         $response = $this->riotConnection->get(
